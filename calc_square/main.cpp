a6a77a687648c8f6c437bcbd62cb9c2ca2f54b8b
Alwin Leerling <dna.leerling@gmail.com>
2019-01-30 21:14:02 +1300

    Added some more logging to see what is going on in this algorithm and checked some refactoring.

--- calc_square/main.cpp
+++ calc_square/main.cpp
@@ -2,17 +2,45 @@
 
 using namespace std;
 
+static int calc_next_square_old( int square )
+{
+    square ^=  0x77;
+
+    if( !( square & 4 ) ) {
+        if( square >= 0x70 )
+            square = ( square + 0x11 ) & 0x73;
+        else
+            square += 0x10;
+    }
+
+    return square;
+}
+
+
+static int calc_next_square_new( int square )
+{
+    square ^= 0x77;
+
+    if( !(square & 4) )
+        square = ( square >= 0x70 ) ? ( square + 0x11 ) & 0x73 : square + 0x10;
+
+    return square;
+}
+
+
+
 int main()
 {
     int square = 0;
     char str[3];
 
     str[2] = '\0';
+
     do {
         str[0] = 'a' + square % 16;
         str[1] = '1' + square / 16;
 
-        cout << "Square = " << str << endl;
+        cout << "Square = " << str << " " << std::hex << square << endl;
 
         square ^=  0x77;
 
@@ -21,9 +49,19 @@ int main()
                 square = ( square + 0x11 ) & 0x73;
             else
                 square += 0x10;
-        }
+        } else
+            cout << "Not going in there" << endl;
 
     } while( square );
 
+/*
+    for( int square = 0; square != 0; square = calc_next_square_new(square) ) {
+
+        str[0] = 'a' + square % 16;
+        str[1] = '1' + square / 16;
+
+        cout << "Square = " << str << " " << std::hex << square << endl;
+    }
+*/
     return 0;
 }
