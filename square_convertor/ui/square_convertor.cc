2771a56dd5ef5eef09cac834b86e69827fe52096
Alwin Leerling <dna.leerling@gmail.com>
2025-09-29 15:35:54 +1300

    Simplified. Don't need usesless view and main can life in controller source file.

--- square_convertor/ui/square_convertor.cc
+++ square_convertor/ui/square_convertor.cc
@@ -17,22 +17,36 @@
  * MA 02110-1301, USA.
  */
 
-#include "controller.h"
-#include "view.h"
- 
 #include "square_convertor_utils.h"
+
+#include <gtkmm.h>
+
+class Controller : public Gtk::ApplicationWindow
+{
+public:
+    Controller();
+
+private:
+    Gtk::Grid * view;
+
+	Gtk::Label *lblSAN;
+	Gtk::Label *lblUint;
+	Gtk::Entry *fldSAN;
+	Gtk::Entry *fldUint;
+};
+
 
 Controller::Controller() : Gtk::ApplicationWindow()
 {
     set_title("Square Convertor");
 
 	std::string ui_file = "square_convertor.ui";  // local build
-	if (!Glib::file_test(ui_file, Glib::FileTest::EXISTS))
+	if( !Glib::file_test(ui_file, Glib::FileTest::EXISTS) )
 		ui_file = std::string(DATADIR) + "/Chess_Stuff/ui/square_convertor.ui";
 
 	auto builder = Gtk::Builder::create_from_file(ui_file);
 
-    view = Gtk::Builder::get_widget_derived<MainView>( builder, "MainView");
+    view = builder->get_widget<Gtk::Grid>( "MainView");
     set_child(*view);
     view->show();
 
@@ -43,4 +57,11 @@ Controller::Controller() : Gtk::ApplicationWindow()
 
 	fldSAN->signal_changed().connect( [this](){ lblUint->set_text( SANtoUInt( fldSAN->get_text() ) ); } );
 	fldUint->signal_changed().connect( [this](){ lblSAN->set_text( UInttoSAN( fldUint->get_text() ) ); } );
+}
+
+int main( int argc, char * argv[] )
+{
+    auto app = Gtk::Application::create();
+
+    return app->make_window_and_run<Controller>( argc, argv );
 }
